871675fe0773fcc52601a63d3788be16d37e2174
attreya01 <attreya01@gmail.com>
2016-06-04 03:17:52 +0530

    Initial commit

---
+++ app/src/main/java/in/attreya/dailylist/AppDailyList.java
@@ -0,0 +1,15 @@
+package in.attreya.dailylist;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class AppDailyList extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        RealmConfiguration configuration=new RealmConfiguration.Builder(this).build();
+        Realm.setDefaultConfiguration(configuration);
+    }
+}
---
+++ app/src/main/java/in/attreya/dailylist/MainActivity.java
@@ -0,0 +1,26 @@
+package in.attreya.dailylist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initBackgroundImage();
+
+    }
+
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(background);
+    }
+}
